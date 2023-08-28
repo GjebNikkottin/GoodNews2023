3fc98fe618e740f85c9e71818b40dd9159ffca6f
Gjeb Nikkottin <gleb-man@mail.com>
2023-08-28 11:37:31 +0500

    Initial Commit

---
+++ GoodNews2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodNews2023
+//
+//  Created by Глеб on 28.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
